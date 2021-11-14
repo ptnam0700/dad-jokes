4519f5b2951890fc44386f8ae330b6aaf5393d06
Nam Pham <ptnam0700@gmail.com>
2021-11-15 00:48:02 +0700

    Add files

---
+++ script.js
@@ -0,0 +1,39 @@
+const jokeEL = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', randomJoke)
+
+randomJoke()
+
+async function randomJoke(){
+    const config = {
+        headers : {
+            'Accept': 'application/json'
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeEL.innerHTML = data.joke
+        
+}
+
+
+
+
+
+// function randomJoke(){
+//     const config = {
+//         headers : {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then(data => {
+//             jokeEL.innerHTML = data.joke
+//         })
+// }
